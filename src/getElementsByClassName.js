ac948a8371ad366155308b2af41cb60fff9e5c5e
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-05-31 23:17:27 -0400

    Finished Bare Min

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,25 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  var elements = [];
+
+  var childNodeSearch = function(nodes) {
+    var list = nodes.classList;
+    var children = nodes.childNodes;
+
+
+    if (list && list.contains(className)) {
+      elements.push(nodes);
+    }
+
+    if (children.length > 0) {
+      for (var i = 0; i < children.length; i++) {
+        childNodeSearch(children[i]);
+      }
+    }
+  };
+
+  childNodeSearch(document.body);
+  return elements;
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,34 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+
+
+var stringifyJSON = function (obj) {
   // your code goes here
-};
+    if (obj === null) {
+      return 'null';
+    } else if (obj === undefined) {
+      return 'undefined';
+    } else if (typeof(obj) === 'string') {
+      return '"' + obj + '"';
+    } else if (Array.isArray(obj)) {
+      var s = '';
+     
+
+      for (var i = 0; i < obj.length; i++) {
+        s = s.concat(stringifyJSON(obj[i]) + ',');
+      }
+      return '[' + s.slice(0, s.length - 1) + ']';
+    } else if (typeof(obj) === 'object') {
+      var s = '';
+      for (var key in obj) {
+        if (obj[key] !== undefined && typeof(obj[key]) !== 'function') {
+          s = s.concat(stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',');
+        }
+      }
+      return '{' + s.slice(0, s.length - 1) + '}';
+    } else {
+      return obj.toString();
+    }
+
+  };
